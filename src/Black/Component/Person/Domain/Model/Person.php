f67edba9828414528235515c73de3986c5fd9ec0
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-02-24 01:49:36 +0100

    Update docblocks

--- src/Black/Component/Person/Domain/Model/Person.php
+++ src/Black/Component/Person/Domain/Model/Person.php
@@ -7,32 +7,64 @@ use Email\EmailAddress;
 
 /**
  * Class Person
- *
- * @author Alexandre Balmes <alexandre@lablackroom.com>
- * @licence http://opensource.org/licenses/mit-license.php MIT
  */
 class Person implements Entity
 {
+    /**
+     * @var PersonId
+     */
     protected $personId;
 
+    /**
+     * @var Name
+     */
     protected $name;
 
+    /**
+     * @var EmailAddress
+     */
     protected $email;
 
+    /**
+     * @var
+     */
     protected $gender;
 
+    /**
+     * @var
+     */
     protected $honorificPrefix;
 
+    /**
+     * @var
+     */
     protected $jobTitle;
 
+    /**
+     * @var
+     */
     protected $telephone;
 
+    /**
+     * @var
+     */
     protected $mobile;
 
+    /**
+     * @var
+     */
     protected $faxNumber;
 
+    /**
+     * @var
+     */
     protected $picture;
 
+    /**
+     * @param PersonId $personId
+     * @param Name $name
+     * @param EmailAddress $email
+     */
     public function __construct(
         PersonId $personId,
         Name $name,
@@ -43,51 +75,81 @@ class Person implements Entity
         $this->email = $email;
     }
 
+    /**
+     * @return PersonId
+     */
     public function getPersonId()
     {
         return $this->personId;
     }
 
+    /**
+     * @return Name
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * @return EmailAddress
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * @return mixed
+     */
     public function getGender()
     {
         return $this->gender;
     }
 
+    /**
+     * @return mixed
+     */
     public function getHonorificPrefix()
     {
        return $this->honorificPrefix;
     }
 
+    /**
+     * @return mixed
+     */
     public function getJobTitle()
     {
         return $this->jobTitle;
     }
 
+    /**
+     * @return mixed
+     */
     public function getTelephone()
     {
         return $this->telephone;
     }
 
+    /**
+     * @return mixed
+     */
     public function getMobile()
     {
         return $this->mobile;
     }
 
+    /**
+     * @return mixed
+     */
     public function getFaxNumber()
     {
         return $this->faxNumber;
     }
 
+    /**
+     * @return mixed
+     */
     public function getPicture()
     {
         return $this->picture;
